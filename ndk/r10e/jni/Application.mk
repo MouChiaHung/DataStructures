b2470153665a03d9109f742f792454456987ee73
MouChiaHung <ifeelgooder@gmail.com>
2019-07-16 16:24:59 +0800

    move ndk to another repo

--- ndk/r10e/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_ABI := all
-APP_STL := gnustl_static
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_OPTIM := debug
--- ndk/r20/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_ABI := all
-#In general, you can only use a static variant of the C++ runtime if you have one and only one shared library in your application.
-APP_STL := c++_static
-NDK_TOOLCHAIN_VERSION := clang
-APP_PLATFORM := android-23
-APP_OPTIM := debug
